a6ad4ba758b42ad4a4130a47aa1c09efdbaa912b
andrew0 <739172+andrew0@users.noreply.github.com>
2019-10-04 14:16:17 -0400

    moved tracejs-vue from tracejs repository

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>
+      <simulation-chart :chart-data="[]" />
+    </div>
+  </div>
+</template>
+
+<script>
+import SimulationChart from './SimulationChart.vue'
+
+export default {
+  components: { SimulationChart }
+}
+</script>
